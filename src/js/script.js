3588d1470fd7c843c77d1d8647aadd055645f714
Bartosz Malolepszy <bartosz.malolepszy@interia.com>
2023-10-10 23:46:38 +0200

    filtred books , 10.2, cw5

--- src/js/script.js
+++ src/js/script.js
@@ -17,7 +17,7 @@
   }
   render();
 
-  //ćwiczenie 2 - dodanie podówjnego kliknięcia do nowej tablicy favbooks
+  //ćwiczenie 2 - dodanie podówjego kliknięcia do nowej tablicy favbooks
   const favoriteBooks = [];
 
   //>>dodanie nasłuchiwacza pętlą na każdą pozycję z książki
@@ -31,20 +31,46 @@
       const bookImage = clickedElement;
       const bookId = bookImage.getAttribute('data-id');
       console.log('zadziałał addEventListener na elemencie bookImage');
-      if (!clickedElement.classList.contains('favorite') &&
-        clickedElement.classList.contains('book__image')) {
+      if (
+        !clickedElement.classList.contains('favorite') &&
+        clickedElement.classList.contains('book__image')
+      ) {
         bookImage.classList.add('favorite');
         favoriteBooks.push(bookId);
-        console.log('dodano książkę do ulubiomnych');
-      } else if 
-      (clickedElement.classList.contains('favorite')) {
+        console.log('dodano książkę do ulubionych');
+      } else if (clickedElement.classList.contains('favorite')) {
         clickedElement.classList.remove('favorite');
         const indexOfBook = favoriteBooks.indexOf(bookId);
         favoriteBooks.splice(indexOfBook, 1);
         console.log('usunięto książkę z ulubionych');
       }
     });
-  }
+
+    // Listener for filters
+    const filters = [];
+    const filtersForm = document.querySelectorAll('.filters');
+
+    filtersForm.addEventListener('click', function (event) {
+      if (
+        event.target.tagName == 'INPUT' &&
+        event.target.type == 'checkbox' &&
+        event.target.name == 'filter'
+      ) {
+        console.log(filtersForm.target.value); //
+
+        if (event.target.checked) {
+          // if clicked add it to filters array
+          filters.push(event.target.value);
+        } else if (!event.target.checked) {
+          // if not, delete
+          filters.splice(filters.indexOf(event.target.value), 1);
+        }
+      }
+      console.log('filters', filters);
+    });
+  } // Dodane zamknięcie funkcji initAction
+
+  //adding filters
 
   initAction();
 
